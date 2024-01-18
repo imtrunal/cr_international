5491459a54bf2c73f4503767a63e112ffa6f6dc1
imparth7 <imparthdudhatra@gmail.com>
2024-01-18 18:59:32 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ const Footer = () => {
     return (
         <div className='bg-[#45270f] text-white flex flex-col items-center justify-center md:gap-5 gap-7 py-10 '>
             <div className="list flex md:flex-row flex-col md:gap-4 gap-2 text-base font-semibold flex-wrap items-center justify-center">
-                <a href="" className='text-white'>Products</a>
-                <a href="" className='text-white'>Catalogue</a>
-                <a href="" className='text-white'>About Us</a>
-                <a href="" className='text-white'>Contact Us</a>
+                {/*<a href="" className='text-white'>Products</a>*/}
+                <a href="/catalogue" className='text-white'>Catalogue</a>
+                <a href="/about" className='text-white'>About Us</a>
+                <a href="/contact" className='text-white'>Contact Us</a>
             </div>
             <div className="copyrights">
                 <p className="text-xl text-center font-bold">© 2024 CR International. All Rights Reserved.</p>
