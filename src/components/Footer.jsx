3b63195b6e172373e76cfcca24a0f418bf59c2b2
imparth7 <imparthdudhatra@gmail.com>
2024-02-15 23:17:32 +0530

    UPDATE:FooterLinks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Footer = () => {
     return (
         <div className='bg-[#45270f] text-white flex flex-col items-center justify-center md:gap-5 gap-7 py-10 '>
             <div className="list flex md:flex-row flex-col md:gap-4 gap-2 text-base font-semibold flex-wrap items-center justify-center">
                 {/*<a href="" className='text-white'>Products</a>*/}
-                <a href="/catalogue" className='text-white'>Catalogue</a>
-                <a href="/about" className='text-white'>About Us</a>
-                <a href="/contact" className='text-white'>Contact Us</a>
+                <Link to={"/catalogue"} className='text-white'>Catalogue</Link>
+                <Link to={"/about"} className='text-white'>About Us</Link>
+                <Link to={"/contact"} className='text-white'>Contact Us</Link>
             </div>
             <div className="copyrights">
                 <p className="text-xl text-center font-bold">© 2024 CR International. All Rights Reserved.</p>
